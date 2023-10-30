39456c76dbf95e735cc9840217d013f7193ea6d3
Dark <mohamed2003elashmawy@gmail.com>
2023-10-31 00:55:54 +0200

    finished file 2.0

--- LZ77.cpp
+++ LZ77.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstring>
+#include <fstream>
 
 using namespace std;
 
-struct LZ77Token {
+struct Token {
     int length;
     int offset;
     char nextChar;
 };
 
-vector<LZ77Token> compress(const string& input) {
-    vector<LZ77Token> compressedData;
+vector<Token> compress(const string& input) {
+    vector<Token> compressedData;
     int inputSize = input.size();
     int currentIndex = 0;
 
     while (currentIndex < inputSize) {
-        LZ77Token token;
+        Token token;
         token.length = 0;
         token.offset = 0;
         token.nextChar = input[currentIndex];
@@ -41,9 +43,9 @@ vector<LZ77Token> compress(const string& input) {
     return compressedData;
 }
 
-string decompress(const vector<LZ77Token>& compressedData) {
+string decompress(const vector<Token>& compressedData) {
     string decompressed;
-    for (const LZ77Token& token : compressedData) {
+    for (const Token& token : compressedData) {
         if (token.length == 0) {
             decompressed += token.nextChar;
         } else {
@@ -59,14 +61,18 @@ string decompress(const vector<LZ77Token>& compressedData) {
 
 int main() {
     
-    string inputText;
-    vector<LZ77Token> compressedData;
-    string decompressedText;
+    fstream file;
+    string input ,inputText, decompressedText;
+    char fileName[100];
+    vector<Token> compressedData;
+
     while (true){
-        cout << "Welcome the LZ77 Compression Algorithm Menu : "<<endl;
-        cout << "1-Compress"<<endl;
-        cout << "2-Decompress"<<endl;
-        cout << "3-Exit"<<endl;
+        cout << "Welcome the LZ77 Menu:\n";
+        cout << "1- Compress Input\n";
+        cout << "2- Decompress Input\n";
+        cout << "3- Compress File\n";
+        cout << "4- Decompress File\n";
+        cout << "5- Exit\n";
         
         int choice;
         cin >> choice;
@@ -74,10 +80,10 @@ int main() {
         switch (choice) {
             case 1:
                 cout << "Input text:" << endl;
-                getline(cin,inputText);
+                getline(cin, inputText);
                 compressedData = compress(inputText);
                 
-                for (const LZ77Token& token : compressedData) {
+                for (const Token& token : compressedData) {
                     cout << "<" << token.offset << "," << token.length << "," << token.nextChar << ">";
                 }
                 cout << endl;
@@ -86,7 +92,54 @@ int main() {
                 decompressedText = decompress(compressedData);
                 cout << "Decompressed Text: " << decompressedText << endl;
                 break;
-            case 3:
+            case 3:                
+                cout << "Enter File Name: \n";
+                cin.getline(fileName, 100);
+                strcat(fileName, ".txt");
+                file.open(fileName, ios::in);
+
+                if(file.is_open()){
+                    input.assign((istreambuf_iterator<char>(file)),(istreambuf_iterator<char>()));
+                }
+                
+                file.close();
+                compressedData = compress(input);
+
+                file.open(fileName,ios::out); // write
+
+                if(file.is_open()){
+                    string output;
+                    for (int i = 0; i < compressedData.size(); i++)
+                    {
+                        output = "<" + to_string(compressedData[i].offset) + "," + to_string(compressedData[i].length) + ">" + compressedData[i].nextChar;
+                        file << output;
+                    }
+                    file.close();
+                }
+
+                break;
+            case 4:
+                cout << "Enter File Name: \n";
+
+                strcat(fileName, "txt");
+                cin.getline(fileName, 100);
+                file.open(fileName, ios::in);
+
+                if(file.is_open()){
+                    input.assign((istreambuf_iterator<char>(file)),(istreambuf_iterator<char>()));
+                }
+                
+                decompressedText = decompress(compressedData);
+
+                file.open(fileName,ios::out); // write
+
+                if(file.is_open()){
+                    file << decompressedText;
+                    file.close();
+                }
+                break;
+                
+            case 5:
                 exit(0);
             default:
                 cout << "Invalid input\n";
