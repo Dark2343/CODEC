14ef98fe5d0499ab3c9a0bf9398aaefa32a1ae9c
Yahya Ehab <yahyaehab1856@gmail.com>
2023-11-10 23:57:40 +0200

    added the decompress function and the writedecompressed and readcompressed function

--- LZW/LZW.java
+++ LZW/LZW.java
@@ -1,8 +1,12 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class LZW{
@@ -12,7 +16,7 @@ public class LZW{
 
     public void compress(File textFile) throws Exception {
         try{
-            String content = readFile(textFile);
+            String content = readTextFile(textFile);
             createDictionary();
             StringBuilder substring = new StringBuilder();
 
@@ -28,19 +32,37 @@ public class LZW{
                     substring.append(c);
                 }
             }
-            writeFile(compressedData, textFile.getName());
+            writeCompressedFile(compressedData, textFile.getName());
         }
         catch (Exception e) {throw e;}
     }
 
     public void decompress(File textFile) throws Exception {
         try{
-            readFile(textFile);
+            List<Integer> compressedData = readCompressedFile(textFile);
             createReverseDictionary();
-        }
-        catch (Exception e) {throw e;}
+            StringBuilder decompressedText = new StringBuilder();
+            int initialCode = compressedData.remove(0);
+            String current = reverseDictionary.get(initialCode);
+            decompressedText.append(current);
+            for (int code : compressedData) {
+                String entry;
+                if (reverseDictionary.containsKey(code)) {
+                    entry = reverseDictionary.get(code);
+                } else if (code == reverseDictionary.size()) {
+                    entry = current + current.charAt(0);
+                } else {
+                    throw new IllegalArgumentException("Invalid compressed data");
+                }
+                decompressedText.append(entry);
+                reverseDictionary.put(reverseDictionary.size(), current + entry.charAt(0));
+                current = entry;
+            }
+            writeDecompressedFile(decompressedText.toString() , textFile.getName());
+
+        } catch (Exception e) {throw e;}
     }
-    
+
     private void createDictionary(){
         for (int i = 0; i < 256; i++) {
             Character c = (char)(i);
@@ -50,12 +72,11 @@ public class LZW{
 
     private void createReverseDictionary(){
         for (int i = 0; i < 256; i++) {
-            Character c = (char)(i);
-            reverseDictionary.put(i, Character.toString(c));
+           reverseDictionary.put(i, String.valueOf((char) i));
         }
     }
-    
-    private String readFile(File textFile) throws Exception {
+
+    private String readTextFile(File textFile) throws Exception {
         try {
             String content = new String();
             Scanner reader = new Scanner(textFile);
@@ -67,11 +88,35 @@ public class LZW{
         } catch (Exception e) {throw e;}
     }
 
-    private void writeFile(ArrayList<Integer> compressedData, String name) throws IOException{
+    private void writeCompressedFile(ArrayList<Integer> compressedData, String name) throws IOException{
         FileWriter output = new FileWriter("Compressed" + name);
         for (int data : compressedData) {
             output.write(data + " ");
         }
+        output.close();
+    }
+    private  List<Integer> readCompressedFile(File textFile) throws Exception {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(textFile));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] codes = line.split(" ");
+                for (String code : codes) {
+                    if (!code.isEmpty()) {
+                        compressedData.add(Integer.parseInt(code));
+                    }
+                }
+            }
+            reader.close();
+            return compressedData;
+        } catch (Exception e) {throw e;}
+
+    }
+
+
+    private void writeDecompressedFile(String decompressedData, String name) throws IOException{
+        FileWriter output = new FileWriter("Decompressed" + name);
+        output.write(decompressedData);
         output.close();
     }
 }
