f03136db56297b05b03ff6d86cacbd983b848e59
Dark <mohamed2003elashmawy@gmail.com>
2023-12-05 02:57:52 +0200

    did shit
    Co-authored-by: Ziad <KZiad@users.noreply.github.com>

--- Vector Quantization/VQ.java
+++ Vector Quantization/VQ.java
@@ -1,5 +1,7 @@
 import java.awt.image.BufferedImage;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
@@ -34,9 +36,21 @@ public class VQ {
         }
     }
 
-    // public void GenerateKSize(){
+    public int GetOptimalKSize(int[][] pixelArray){
+        int width = pixelArray.length, height = pixelArray[0].length;
+        int kSize = 0;
 
-    // }
+        if(width == height){
+            kSize = width / 2;
+        }
+        else if(width > height){
+            kSize = height / 2;
+        }
+        else{
+            kSize = width / 2;
+        }
+        return kSize;
+    }
 
     public void GenerateCodeBook(int[][] pixelArray, int kSize){
         int width = pixelArray.length, height = pixelArray[0].length;
@@ -55,6 +69,8 @@ public class VQ {
                 codeBook.add(entry);
             }
         }
+        
+        GetAverageEntry(kSize);
     }
 
     public void GetAverageEntry(int kSize){
@@ -74,36 +90,65 @@ public class VQ {
                 averageEntry[i][j] /= kSize;
             }
         }
+
+        SplitCodeBook(averageEntry, kSize);
     }
     
-    public void SplitCodeBook(int[][] averageEntry, int kSize){
-        int[][] low = new int[kSize][kSize], high = low;
-        int averageLow = 0, averageHigh = 0;
-
-        for(int i = 0; i < averageEntry.length; i++){
-            for(int j = 0; j < averageEntry[0].length; j++){
-                low[i][j] = (int) Math.floor(averageEntry[i][j]);
-                averageLow += low[i][j]; 
-                high[i][j] = (int) Math.ceil(averageEntry[i][j]);
-                averageHigh += high[i][j]; 
-            }
-        }
-
+    public void SplitCodeBook(float[][] averageEntry, int kSize){
+        
+        
     }
 
     public void compress(File imageFile) throws Exception{
         try{
             int[][] pixelArray = ProcessGrayScaleImage(imageFile);
-            GenerateCodeBook(pixelArray, 2);
-
+            int kSize = GetOptimalKSize(pixelArray);
+            GenerateCodeBook(pixelArray, kSize);
         }
         catch(Exception e){
             throw e;
         }
     }
 
+    public void saveCompressedFile(int[][] compressedImage) throws Exception{
+        saveCodeBook();
+        try (DataOutputStream dataOut = new DataOutputStream(new FileOutputStream("compressedImage.bin"))){
+            // size of the image
+            dataOut.writeShort(compressedImage.length);
+            dataOut.writeShort(compressedImage[0].length);
+            for(int i = 0; i < compressedImage.length; i++){
+                for(int j = 0; j < compressedImage[0].length; j++){
+                    // Compressed image value
+                    dataOut.writeByte(compressedImage[i][j]);
+                }
+            }
+        }   catch (Exception e) {
+            throw e;
+        }
+    }
 
-    public void decompress(File imageFile) throws Exception{
+    private void saveCodeBook() throws Exception {
+        try (DataOutputStream dataOut = new DataOutputStream(new FileOutputStream("compressedImage.bin"))){
+            // Number of entries in the code book
+            dataOut.writeShort(codeBook.size());
+            for(int i = 0; i < codeBook.size(); i++){
+                // Code book entry number
+                dataOut.writeByte(i);
+                for(int j = 0; j < codeBook.get(i).length; j++){
+                    for(int k = 0; k < codeBook.get(i)[j].length; k++){
+                        // Code book entry value
+                        dataOut.writeByte(codeBook.get(i)[j][k]);
+                    }
+                }
+            }
+        }   catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void decompress(File compressedFile) throws Exception{
+        int[][] compressedImage = readCompressedFile(compressedFile);
         
+
     }
 }
