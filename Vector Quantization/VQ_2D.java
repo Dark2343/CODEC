3be6dab0bba53296b7c811e3ad11a6bb910f063e
Dark <mohamed2003elashmawy@gmail.com>
2023-12-11 18:53:05 +0200

    finished grayscaled compression

--- Vector Quantization/VQ_2D.java
+++ Vector Quantization/VQ_2D.java
@@ -3,13 +3,15 @@ import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import java.io.DataOutputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.awt.image.BufferedImage;
 
 public class VQ_2D {
 
-    ArrayList<int[][]> clusters = new ArrayList<int[][]>();
-    ArrayList<int[][]> codeBook = new ArrayList<int[][]>();
-
+    int KSIZE;
+    ArrayList<float[][]> clusters = new ArrayList<float[][]>();
+    ArrayList<float[][]> codeBook = new ArrayList<float[][]>();
+    
     public int[][] ProcessGrayScaleImage(File imageFile) throws Exception {
         try{
             BufferedImage img = ImageIO.read(imageFile);
@@ -22,7 +24,7 @@ public class VQ_2D {
                     
                     // Get the RGB value of the pixel
                     int rgb = img.getRGB(i, j);
-                                        
+                    
                     // Extract the grayscale value (assuming it's a single channel)
                     int grayscaleValue = (rgb >> 16) & 0xFF;
                     
@@ -36,17 +38,17 @@ public class VQ_2D {
             throw e;
         }
     }
-
-    public void GenerateClusters(int[][] pixelArray, int kSize){
+    
+    public void GenerateClusters(int[][] pixelArray){
         int width = pixelArray.length, height = pixelArray[0].length;
-
-        for(int i = 0; i < width; i += kSize){
-            for(int j = 0; j < height; j += kSize){
+        
+        for(int i = 0; i < width; i += KSIZE){
+            for(int j = 0; j < height; j += KSIZE){
                 
-                int entry[][] = new int[kSize][kSize];
+                float entry[][] = new float[KSIZE][KSIZE];
                 
-                for(int x = 0; x < kSize; x++){
-                    for (int y = 0; y < kSize; y++) {
+                for(int x = 0; x < KSIZE; x++){
+                    for (int y = 0; y < KSIZE; y++) {
                         entry[x][y] = pixelArray[x + i][y + j]; 
                     }
                 }
@@ -54,60 +56,179 @@ public class VQ_2D {
                 clusters.add(entry);
             }
         }
-        
-        GetAverageEntry(kSize);
     }
-
-    public void GetAverageEntry(int kSize){
+    
+    public float[][] GetAverageEntry(ArrayList<float[][]> clusterGroup){
         
-        float[][] averageEntry = new float[kSize][kSize];
-
-        for(int i = 0; i < clusters.size(); i++){
-            for(int j = 0; j < clusters.get(i).length; j++){
-                for(int k = 0; k < clusters.get(i)[j].length; k++){
-                    averageEntry[j][k] += clusters.get(i)[j][k];
+        float[][] averageEntry = new float[KSIZE][KSIZE];
+        
+        for(int i = 0; i < clusterGroup.size(); i++){
+            for(int j = 0; j < clusterGroup.get(i).length; j++){
+                for(int k = 0; k < clusterGroup.get(i)[j].length; k++){
+                    averageEntry[j][k] += clusterGroup.get(i)[j][k];
                 }
             }
         }
-
+        
         for(int i = 0; i < averageEntry.length; i++){
             for(int j = 0; j < averageEntry[0].length; j++){
-                averageEntry[i][j] /= kSize;
+                averageEntry[i][j] /= clusterGroup.size();
             }
         }
-
-        SplitClusters(averageEntry, kSize);
+        return averageEntry;
     }
+    
+    public void SplitClusters(float[][] averageEntry){
+        codeBook.add(averageEntry);
+        while (codeBook.size() < KSIZE) {
+            ArrayList<float[][]> tempBook = new ArrayList<float[][]>();
+            ArrayList<ArrayList<float[][]>> nearestVectors = new ArrayList<ArrayList<float[][]>>();
+            
+            for(float[][] cluster : codeBook){
+                float[][] low = new float[KSIZE][KSIZE];
+                float[][] high = new float[KSIZE][KSIZE];
+                
+                for(int i = 0; i < cluster.length; i++){
+                    for(int j = 0; j < cluster[0].length; j++){
+                        low[i][j] = (float) Math.floor(cluster[i][j]);
+                        high[i][j] = (float) Math.ceil(cluster[i][j]);
+                    }
+                }
+                tempBook.add(low);
+                tempBook.add(high);
+            }
+            nearestVectors = CalculateDistances(tempBook);
+            codeBook.clear();
 
+            for (int i = 0; i < nearestVectors.size(); i++) {
+                codeBook.add(GetAverageEntry(nearestVectors.get(i)));
+            }
+        }
+    }
+    
+    public ArrayList<ArrayList<float[][]>> CalculateDistances(ArrayList<float[][]> tempBook){
+        ArrayList<ArrayList<float[][]>> nearestVectors = new ArrayList<ArrayList<float[][]>>();
 
-    public void SplitClusters(float[][] averageEntry, int kSize){
-        
+        for (int i = 0; i < tempBook.size(); i++) {
+            nearestVectors.add(new ArrayList<float[][]>());
+        }
+
+        for(float[][] cluster : clusters){
+            int minDistance = Integer.MAX_VALUE;
+            int minDistanceCluster = -1;
+            
+            for(int i = 0; i < tempBook.size(); i++){
+                int tempDistance = 0;
+                
+                for(int x = 0; x < KSIZE; x++){
+                    for(int y = 0; y < KSIZE; y++){
+                        tempDistance += Math.abs(cluster[x][y] - tempBook.get(i)[x][y]);
+                    }
+                }
+                minDistanceCluster = (minDistance > tempDistance) ? i : minDistanceCluster;
+                minDistance = (minDistance > tempDistance) ? tempDistance : minDistance;
+            }
+            nearestVectors.get(minDistanceCluster).add(cluster);
+        }
+        return nearestVectors;
+    }
+    
+    public int[][] OverwriteImage(int width, int height) {
+        int[][] newPixelArray = new int[width][height];
+    
+        for (int i = 0; i < clusters.size(); i++) {
+            float[][] cluster = clusters.get(i);
+    
+            // Find the closest codeBook entry
+            int minDistance = Integer.MAX_VALUE;
+            int minDistanceCluster = -1;
+    
+            for (int j = 0; j < codeBook.size(); j++) {
+                int tempDistance = 0;
+    
+                for (int x = 0; x < KSIZE; x++) {
+                    for (int y = 0; y < KSIZE; y++) {
+                        tempDistance += Math.abs(cluster[x][y] - codeBook.get(j)[x][y]);
+                    }
+                }
+    
+                if (tempDistance < minDistance) {
+                    minDistance = tempDistance;
+                    minDistanceCluster = j;
+                }
+            }
+    
+            // Set new pixel array values based on the chosen codeBook entry
+            float[][] chosenCodeBookEntry = codeBook.get(minDistanceCluster);
+            int xOffset = (i % (width / KSIZE)) * KSIZE;  // Corrected calculation
+            int yOffset = (i / (width / KSIZE)) * KSIZE;  // Corrected calculation
+    
+            for (int x = 0; x < KSIZE; x++) {
+                for (int y = 0; y < KSIZE; y++) {
+                    newPixelArray[x + xOffset][y + yOffset] = (int) chosenCodeBookEntry[x][y];
+                }
+            }
+        }
+    
+        return newPixelArray;
     }
 
     public void compress(File imageFile, int kSize) throws Exception{
         try{
+            KSIZE = kSize;
             int[][] pixelArray = ProcessGrayScaleImage(imageFile);
-            GenerateClusters(pixelArray, kSize);
+            GenerateClusters(pixelArray);
+            SplitClusters(GetAverageEntry(clusters));
+
+            BufferedImage img = ImageIO.read(imageFile);
+            int width = img.getWidth(), height = img.getHeight();
+
+            pixelArray = OverwriteImage(width, height);
+            String name = imageFile.getName().replaceFirst("[.][^.]+$", "");
+            WriteCompressedImage(pixelArray, name);
         }
         catch(Exception e){
             throw e;
         }
     }
 
-    public void saveCompressedFile(int[][] compressedImage) throws Exception{
-        // saveCodeBook();
-        try (DataOutputStream dataOut = new DataOutputStream(new FileOutputStream("compressedImage.bin"))){
-            // size of the image
-            dataOut.writeShort(compressedImage.length);
-            dataOut.writeShort(compressedImage[0].length);
-            for(int i = 0; i < compressedImage.length; i++){
-                for(int j = 0; j < compressedImage[0].length; j++){
-                    // Compressed image value
-                    dataOut.writeByte(compressedImage[i][j]);
-                }
+    // FOR BINARY FILE OUTPUT
+    
+    // public void saveCompressedFile(int[][] compressedImage) throws Exception{
+    //     // saveCodeBook();
+    //     try (DataOutputStream dataOut = new DataOutputStream(new FileOutputStream("compressedImage.bin"))){
+    //         // size of the image
+    //         dataOut.writeShort(compressedImage.length);
+    //         dataOut.writeShort(compressedImage[0].length);
+    //         for(int i = 0; i < compressedImage.length; i++){
+    //             for(int j = 0; j < compressedImage[0].length; j++){
+    //                 // Compressed image value
+    //                 dataOut.writeByte(compressedImage[i][j]);
+    //             }
+    //         }
+    //     }   catch (Exception e) {
+    //         throw e;
+    //     }
+    // }
+
+
+    // FOR COMPRESSED IMAGE OUTPUT
+    public void WriteCompressedImage(int[][] pixelArray, String name) {
+        String path = System.getProperty("user.dir") + "\\"+ name + "_Grayscaled.png";
+        BufferedImage image = new BufferedImage(pixelArray.length, pixelArray[0].length, BufferedImage.TYPE_BYTE_GRAY);
+        for (int x = 0; x < pixelArray.length; x++) {
+            for (int y = 0; y < pixelArray[0].length; y++) {
+                int grayValue = pixelArray[x][y];
+                int pixelValue = (grayValue << 16) | (grayValue << 8) | grayValue;
+                image.setRGB(y, x, pixelValue);
             }
-        }   catch (Exception e) {
-            throw e;
+        }
+
+        File ImageFile = new File(path);
+        try {
+            ImageIO.write(image, "png", ImageFile);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
